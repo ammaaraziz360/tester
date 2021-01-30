9e98692ddca5f3280ce346ca600c03ac4fa1d0bc
Ammaar Mohammed <ammaaraziz360@gmail.com>
2021-01-30 02:55:12 -0600

    sql

--- main.py
+++ main.py
@@ -7,15 +7,34 @@ def create_connection(host_name, user_name, user_password, db_name):
 
     connection = mysql.connector.connect(
         host = host_name,
-        user = user_name
+        user = user_name,
         passwd = user_password,
-        datbase = db_name
+        database = db_name
     )
     
     print("Connection to mySQL DB was succesful")
 
-    return connector
+    return connection
 
+# call function to open connection
 # aws endpoint
 # username
-connection = create_connection("","","","")
+connection = create_connection("axaam-db.cgzvo3rucizz.us-east-1.rds.amazonaws.com","ammaaraziz360","hymvim-minnib-5torKa","cis3368_db")
+
+# execute a read query on the DB - to get evry row form the users table
+# we need a query
+query = "SELECT * FROM users"
+# place to store results
+result = None
+# cursor to operate
+cursor = connection.cursor()
+
+cursor.execute(query)
+result = cursor.fetchall()
+
+print(result)
+
+
+
+
+
